76be19a1083b4b62d6205a3ab85bf52a06ddeab1
Johannes Wülk <johnwuelk@users.noreply.github.com>
2021-06-11 14:01:58 -0400

    Update install.sh

--- debian/install.sh
+++ debian/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt install wireless-tools network-manager wpasupplicant xorg git nnn i3 vim acpi terminator xterm lm-sensors htop rsync 
-ntfs-3g 
+ntfs-3g ibus-m17n omegat 
 #for TUI display manager: https://github.com/nullgemm/ly
 
 #usermod -aG sudo johann
